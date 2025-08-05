4144c2e7e19d330cb63913739020e3a77bdc43fa
Yogesh7030 <yogesh.07bantiya@gmail.com>
2025-08-05 19:22:46 +0200

    Initial commit: Arcade Pong Game

---
+++ Scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.display_score()
+
+    def display_score(self):
+        self.clear()
+        self.goto(0, -300)
+        self.write("|\n" * 100, align="center", font=("Courier", 20, "normal"))
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
---
+++ main.py
@@ -0,0 +1,89 @@
+import time
+from turtle import Screen
+from Arcade_participants import Arcade
+from Scoreboard import Scoreboard
+from ball import Ball
+
+Controller1_key_up = "Up"
+Controller1_key_down = "Down"
+Controller2_key_up = "W"
+Controller2_key_down = "S"
+Number_of_points = 10
+
+STARTING_POSITIONS_PARTICIPANT1 = (350, 0)
+STARTING_POSITIONS_PARTICIPANT2 = (-350,0)
+
+my_screen = Screen()
+my_screen.bgcolor("black")
+my_screen.setup(width=800, height=600)
+my_screen.title("Arcade Game")
+game_is_on = True
+my_screen.tracer(0)
+
+scoreboard = Scoreboard()
+arcade = Arcade(STARTING_POSITIONS_PARTICIPANT1)
+arcade2 = Arcade(STARTING_POSITIONS_PARTICIPANT2)
+ball = Ball()
+
+
+
+my_screen.listen()
+my_screen.onkey(arcade.up, Controller1_key_up)
+my_screen.onkey(arcade.down, Controller1_key_down)
+
+my_screen.onkey(arcade2.up, Controller2_key_up)
+my_screen.onkey(arcade2.down, Controller2_key_down)
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    my_screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    if ball.distance(arcade) < 60 and ball.xcor() > 320 or ball.distance(arcade2) < 60 and ball.xcor() > -320:
+        ball.bounce_back()
+
+    if ball.xcor() > 370:
+        scoreboard.l_score += 1
+        scoreboard.display_score()
+        ball.reset()
+
+    if ball.xcor() < -370:
+        scoreboard.r_score += 1
+        scoreboard.display_score()
+        ball.reset()
+
+    if scoreboard.l_score == Number_of_points or scoreboard.r_score == Number_of_points:
+        game_is_on = False
+
+
+
+
+
+
+
+# while game_is_on:
+
+#
+    # arcade2.move()
+    # arcade.move()
+    # arcade.set_heading()
+    # arcade2.set_heading()
+
+# turtle = Turtle("square")
+# turtle.shapesize(3,1)
+# turtle.penup()
+# turtle.color("White")
+# turtle.goto(-280, 0)
+
+# while game_is_on:
+
+    # turtle.setheading(90)
+    # turtle.forward(20)
+
+
+
+
+my_screen.exitonclick()
